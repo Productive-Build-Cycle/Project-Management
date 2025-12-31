fa2d3120e3bdfa3613cda9ef499c10db3bcb000a
nasibehash <s.nasibehashtari@gmail.com>
2025-12-31 17:24:02 +0330

    Feature: Add Update project api

---
+++ src/ProjectManagement.Application/DTOs/UpdateProjectDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectManagement.Application.DTOs;
+
+using System.ComponentModel.DataAnnotations;
+
+public class UpdateProjectDto
+{
+    [Required] public Guid Id { get; set; }
+
+    [Required] [MaxLength(200)] public string Title { get; set; }
+
+    [MaxLength(1000)] public string Description { get; set; }
+
+    public string TeamId { get; set; }
+
+    [Required] public DateTime DeadlineTime { get; set; }
+}
