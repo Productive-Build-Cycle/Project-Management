522f32bec5df3f9f2b0ac15824cfa0a69d34abac
shirin <sherize1373@gmail.com>
2026-01-01 16:40:09 +0300

    Implement EndProject Command & Handler

---
+++ src/ProjectManagement.Application/Features/Commands/EndProject/EndProjectCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Application.Features.Commands.EndProject
+{
+    public record EndProjectCommand(
+    Guid ProjectId,
+    DateTime EndTime
+) : IRequest;
+}
---
+++ src/ProjectManagement.Application/Features/Commands/EndProject/EndProjectHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ProjectManagement.Application.Interfaces.Persistence;
+using ProjectManagement.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Application.Features.Commands.EndProject
+{
+    public class EndProjectHandler(
+    IProjectRepository projectRepository,
+    IUnitOfWork unitOfWork)
+    : IRequestHandler<EndProjectCommand>
+    {
+        private readonly IProjectRepository _projectRepository = projectRepository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task Handle(
+            EndProjectCommand request,
+            CancellationToken cancellationToken)
+        {
+            var project = await _projectRepository
+                .GetByIdAsync(request.ProjectId, cancellationToken);
+
+            if (project is null)
+                throw new Exception("Project not found");
+
+            project.Finish(request.EndTime);
+
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
